bfb3777d0efd138cc8c71a6a9be5350b6d61e4ad
DeltaCoderr <51528076+DeltaCoderr@users.noreply.github.com>
2020-07-19 16:58:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const followers = document.getElementById('followers');
 
+async function getFollowers(profileId) {
+    const url      = `https://instagram.com/${profileId}/?__a=1`;
+    const response = await fetch(url);
+    const data     = await response.json();
+
+    return data.graphql.user.edge_followed_by.count;
+}
+
+async function setFollowers(profileId) {
+    followers.textContent = await getFollowers(profileId);
+}
+
+setInterval(() => setFollowers(''), 1000);
